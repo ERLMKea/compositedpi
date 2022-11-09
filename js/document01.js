458251471f674016389e1b0b273c627d67731e60
ERLMKea <erlm@kea.dk>
2022-11-09 09:37:01 +0100

    now with out

--- js/document01.js
+++ js/document01.js
@@ -6,11 +6,16 @@ class Document {
     sign(signature) {
         this.signature = signature
     }
+    out() {
+        console.log("Im a Document title=" + this.title)
+        //console.log(this)
+    }
 }
 
 class DocumentComposite {
     constructor(title) {
         this.items = []
+        this.title = title
     }
 
     add(item) {
@@ -22,18 +27,35 @@ class DocumentComposite {
             doc.sign(signature)
         })
     }
+
+    out() {
+        console.log("I am Composite title= " + this.title)
+        this.items.forEach((doc) => {
+            doc.out()
+        })
+    }
 }
 
 //simpel tree structucture:   root , mental physical, mental - doc1 doc2 , physical phys1 phys2
 
 const root = new DocumentComposite("root")
 const mental = new DocumentComposite("mental")
+const physical = new DocumentComposite("physical")
 root.add(mental)
+root.add(physical)
 
 const doc1 = new Document("Im Doc1")
 const doc2 = new Document("Im Doc2")
 mental.add(doc1)
 mental.add(doc2)
 
+const phys1 = new Document("Im Phys1")
+const phys2 = new Document("Im Phys2")
+physical.add(phys1)
+physical.add(phys2)
+
+
 root.sign("DonaldDuck")
-console.log(root)
+doc1.out()
+mental.out()
+
