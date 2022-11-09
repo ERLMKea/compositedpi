a7fa8dec38374b8e9ba79c915288e50106feafcc
ERLMKea <erlm@kea.dk>
2022-11-09 10:02:10 +0100

    now with remove

--- js/document01.js
+++ js/document01.js
@@ -1,14 +1,19 @@
 class Document {
     constructor(title) {
         this.title = title
+        this.parent = null
         this.signature = null
     }
     sign(signature) {
         this.signature = signature
     }
     out() {
-        console.log("Im a Document title=" + this.title)
+        console.log("Im a Document title=" + this.title + " parent=" + this.parent.title)
         //console.log(this)
+    }
+
+    remove() {
+        this.parent.removeMe(this.title)
     }
 }
 
@@ -16,9 +21,11 @@ class DocumentComposite {
     constructor(title) {
         this.items = []
         this.title = title
+        this.parent = null
     }
 
     add(item) {
+        item.parent = this
         this.items.push(item)
     }
 
@@ -29,19 +36,29 @@ class DocumentComposite {
     }
 
     out() {
-        console.log("I am Composite title= " + this.title)
+        console.log("I am Composite title= " + this.title + " parent=" + this.parent.title)
         this.items.forEach((doc) => {
             doc.out()
         })
+    }
+
+    removeMe(title) {
+        this.items = this.items.filter(obj => obj.title != title)
+    }
+
+    remove() {
+        this.parent.removeMe(this.title)
     }
 }
 
 //simpel tree structucture:   root , mental physical, mental - doc1 doc2 , physical phys1 phys2
 
 const root = new DocumentComposite("root")
+root.parent = root
 const mental = new DocumentComposite("mental")
 const physical = new DocumentComposite("physical")
 root.add(mental)
+
 root.add(physical)
 
 const doc1 = new Document("Im Doc1")
@@ -59,3 +76,9 @@ root.sign("DonaldDuck")
 doc1.out()
 mental.out()
 
+doc1.remove()
+console.log("xxxxxxxxx")
+mental.out()
+console.log("yyyyyyyyy")
+mental.remove()
+root.out()
